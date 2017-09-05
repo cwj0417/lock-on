6341ecc8890c412ccbb16c3ba2043ea18a2674f6
chenwj <731028571@qq.com>
2017-09-05 17:54:25 +0800

    update

--- src/main/index.js
+++ src/main/index.js
@@ -112,13 +112,7 @@ app.on('activate', () => {
   }
 })
 
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
+// auto-updater
 
 autoUpdater.on('update-downloaded', () => {
   autoUpdater.quitAndInstall()
